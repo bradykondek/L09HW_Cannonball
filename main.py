6f2e9c69e79dc962048545929c7f640c29865f52
Brady Kondek <brady.kondek@bradykondek.com>
2024-10-03 05:07:22 -0400

    Implement Crazyball class and modify Cannonball class

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from math import sin, cos
 from matplotlib import pyplot as plt
+import random
+
+# Print Interface class
+class PrintInterface:
+
+    # Print function for this class
+    def mainPrint(self, x, y):
+        
+        print("The ball is at (%.1f, %.1f)" % (self.getX(), self.getY()))
+        plt.scatter(x, y)
+        plt.pause(.01)
 
 ## Represent a cannonball, tracking its position and velocity.
 #
 class Cannonball:
+    
+    # Establish HAS-A relationship with PrintInterface class
+    printInterface_cls = PrintInterface
+    
     ## Create a new cannonball at the provided x position.
     #  @param x the x position of the ball
     #
@@ -12,6 +27,7 @@ class Cannonball:
         self._y = 0
         self._vx = 0
         self._vy = 0
+        self.printInterface = self.printInterface_cls  # relate class to this class
 
     ## Move the cannon ball, using its current velocities.
     #  @param sec the amount of time that has elapsed.
@@ -47,16 +63,45 @@ class Cannonball:
         self.move(0.1, user_grav)
 
         while self.getY() > 1e-14:
-            print("The ball is at (%.1f, %.1f)" % (self.getX(), self.getY()))
-
-            plt.scatter(self.getX(), self.getY())
-            plt.pause(.01)
+            self.printInterface.mainPrint(self._vx, self._vy)
             self.move(0.1, user_grav)
 
+# Crazyball class
+class Crazyball(Cannonball):
+    
+    # Move function (modified for this class)
+    def move(self, sec, grav=9.81):
+        
+        #super().__init__(_x, _y, _vx, _vy)
+        
+        # Calculate the change in posiiton based on velocity
+        dx = self._vx * sec
+        dy = self._vy * sec
 
+        # Update the vertical velocity to account for gravity
+        self._vy = self._vy - grav * sec
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+        # Update the object's positon
+        self._x = self._x + dx
+        self._y = self._y + dy
+
+        # Generate a random value
+        self.rand_q = random.randrange(0, 10)
+
+        # Update x with random number
+        if(self.getX() < 400):  # if x is less than 400
+
+            # Adjust x by random number
+            self._x += self.rand_q
+
+        # Update y with random number
+        if(self.getY() < 400):  # if y is less than 400
+
+            # Adjust y by random number
+            self._y += self.rand_q
+
+# Main function
+def main():
     ##
     #  Demonstrate the cannonball class.
     #
@@ -67,5 +112,6 @@ if __name__ == '__main__':
     c = Cannonball(0)
     c.shoot(angle, v, 9.81)
 
-
-
+# Run program
+if __name__ == '__main__':
+    main()  # run main function
